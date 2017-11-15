438ac7fd2f62ea5100960d731a29a7b884efae7f
endless042 <letyouknow@naver.com>
2017-11-15 18:45:59 +0900

    1
    1

---
+++ HelloWorld/src/com/sist/HelloJava.java
@@ -0,0 +1,11 @@
+package com.sist;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");
+
+	}
+
+}
---
+++ HelloWorld/src/com/sist/HelloJava4.java
@@ -0,0 +1,17 @@
+package com.sist;
+
+public class HelloJava4 {
+
+	public static void main(String[] args) {
+		int i;
+		i=10;
+		
+		double d=20.0;
+		
+		System.out.println(i);
+		System.out.println(d);
+		
+
+	}
+
+}
